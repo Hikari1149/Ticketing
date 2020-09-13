10b580420270b949996f4bde78c4d13bf4bbe24d
Hikar1149 <122094854@qq.com>
2020-09-13 21:14:41 +0800

    [+]: require-auth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,15 @@
 import express from 'express';
-
+import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi There');
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+  try {
+    res.send({ currentuser: req.currentUser || null });
+  } catch (err) {
+    res.send({ currentuser: null });
+  }
 });
 
 export { router as currentUserRouter };
