0e025e806f9fa3cadcab9ec2da5f4c034cb7e8de
Hikar1149 <122094854@qq.com>
2020-09-30 11:30:30 +0800

    [+]: abstract listener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 console.clear();
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -7,9 +7,22 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
+
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service');
+
   const subscrption = stan.subscribe(
     'ticket:created',
-    'orders-service-queue-group'
+    'orders-service-queue-group',
+    options
   );
 
   subscrption.on('message', (msg: Message) => {
@@ -18,5 +31,51 @@ stan.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+    msg.ack();
   });
 });
+
+process.on('SIGNIT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+
+  protected ackWait = 5 * 1000; // number of seconds this listener has to ack a message
+
+  private client: Stan;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  suscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+  // set up the suscription
+  listen() {
+    const suscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.suscriptionOptions()
+    );
+    suscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+      const parseData = this.parseMessage(msg);
+      this.onMessage(parseData, msg);
+    });
+  }
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf-8'));
+  }
+}
