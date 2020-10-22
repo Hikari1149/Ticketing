48420cacec352a255ec3232fd68f208767afec17
Hikar1149 <122094854@qq.com>
2020-10-22 21:56:17 +0800

    fix: use mock stripe

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -7,7 +7,7 @@ import { OrderStatus } from '@hitickets/common';
 import { stripe } from '../../stripe';
 import { Payment } from '../../models/payment';
 
-//jest.mock('../../stripe');
+jest.mock('../../stripe');
 
 it(`return a 404 when purchasing an order that does not exist`, async () => {
   await request(app)
@@ -82,19 +82,19 @@ it(`return a 201 with valid inputs`, async () => {
     })
     .expect(201);
 
-  // const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
-  // expect(chargeOptions.source).toEqual('tok_visa');
-  // expect(chargeOptions.currency).toEqual('usd');
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual('tok_visa');
+  expect(chargeOptions.currency).toEqual('usd');
 
-  const stripeCharges = await stripe.charges.list({ limit: 50 });
-  const stripeCharge = stripeCharges.data.find((charge) => {
-    return charge.amount === price * 100;
-  });
-  expect(stripeCharge).toBeDefined();
+  // const stripeCharges = await stripe.charges.list({ limit: 50 });
+  // const stripeCharge = stripeCharges.data.find((charge) => {
+  //   return charge.amount === price * 100;
+  // });
+  // expect(stripeCharge).toBeDefined();
 
-  const payment = await Payment.findOne({
-    orderId: order.id,
-    stripeId: stripeCharge!.id,
-  });
-  expect(payment).not.toBeNull();
+  // const payment = await Payment.findOne({
+  //   orderId: order.id,
+  //   stripeId: stripeCharge!.id,
+  // });
+  // expect(payment).not.toBeNull();
 });
